936b99aaed0f43482925dd4d68e1e18b583e030a
LAPTOP-1E1JPDL3\User <ekarat.suk@hotmail.com>
2024-06-28 22:45:51 +0700

    SET UP PROJECT

--- ReceivedTerminal/Program.cs
+++ ReceivedTerminal/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+
+class Receiver
+{
+    static async Task Main(string[] args)
+    {
+        TcpListener listener = new TcpListener(IPAddress.Any, 5714);
+
+        try
+        {
+
+        }catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
